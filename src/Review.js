1d39f846812f7e269c2265653b90de57a65a6046
Jerin Ignatious Palliparambil <jerinignatious1998@gmail.com>
2021-04-12 12:04:21 +0530

    RMP-103 | Nav added

--- src/Review.js
+++ src/Review.js
@@ -5,6 +5,24 @@ import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
 const Review = () => {
   const [index, setIndex] = useState(0);
   const {name, job, image, text} = people[index];
+
+  const prevPerson = () => {
+    setIndex((index) => {
+      let newIndex = (index - 1);
+      if(newIndex < 0){
+        newIndex = people.length-1;
+      }
+    return newIndex;
+    });
+  };
+
+  const nextPerson = () => {
+    setIndex((index) => {
+      let newIndex = (index + 1)%people.length;
+      return newIndex;
+    });
+  };
+
   return (
     <article className='review'>
       <div className='img-container'>
@@ -16,6 +34,17 @@ const Review = () => {
       <h4 className='author'>{name}</h4>
       <p className='job'>{job}</p>
       <p className='info'>{text}</p>
+      <div className='button-container'>
+        <button className='prev-btn' onClick = {prevPerson}>
+          <FaChevronLeft/>
+        </button>
+        <button className='next-btn' onClick = {nextPerson}>
+          <FaChevronRight/>
+        </button>
+      </div>
+      <button className='random-btn'>
+        surprise me
+      </button>
     </article>
   );
 };
